aea488ef7681187e3765f053dd6e718ad08aff7b
Kanini-esther <esther.njeri.kanini@gmail.com>
2020-10-19 09:14:58 +0300

    first commit

---
+++ app/src/main/java/com/example/quizapp/Contants.kt
@@ -0,0 +1,9 @@
+package com.example.quizapp
+object Contants {
+    fun getQuestions(): ArrayList<Question>{
+        val QuestionsList = ArrayList<Question>()
+
+        val Que1 = Question(id: 1, question: "Which ")
+
+    }
+}
